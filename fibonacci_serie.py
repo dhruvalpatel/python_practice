8ec9e84c02526c4025e989c09daf225625ba1d0c
vikas-bhargav <vikasbhargav0@gmail.com>
2023-08-13 12:16:47 +0530

    Added some questions

---
+++ fibonacci_serie.py
@@ -0,0 +1,32 @@
+"""Fibonacci series using generator and recursion ."""
+
+
+def fibonacci_series_using_generator():
+    """Create a Fibonacci series using generator"""
+    x, y = 0, 1
+    while True:
+        yield x
+        x, y = y, x + y
+
+
+def fibonacci_series_using_recursion(n):
+    """Create a Fibonacci series using recursion"""
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_series_using_recursion(n - 1) + fibonacci_series_using_recursion(n - 2)
+
+
+def main():
+    # Generator
+    fibo_obj = fibonacci_series_using_generator()  # create object
+    for num in range(5):
+        print(next(fibo_obj))  # iterate object
+
+    # Recursion
+    for i in range(5):
+        print(fibonacci_series_using_recursion(i))
+
+
+if __name__ == '__main__':
+    main()
